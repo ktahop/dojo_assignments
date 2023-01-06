e5ea2f502c582d2adf2e794dafb5694cd213f455
Kristian Tahop <ktahop@gmail.com>
2023-01-06 14:41:42 -0500

    week 1 python

--- python/fundamentals/fundamentals/for_loops_basic_1/for_loop_basic1.py
+++ python/fundamentals/fundamentals/for_loops_basic_1/for_loop_basic1.py
@@ -7,7 +7,7 @@ for int in range(5,1001):
   if int%5 == 0:
     print(int)
 
-#3
+# 3
 for int in range(1, 1001):
   if int%10 == 0:
     print("Coding Dojo")
@@ -16,18 +16,18 @@ for int in range(1, 1001):
   else:
     print(int)
 
-#4
+# 4
 sum = 0
 for int in range(0,500001):
   if int%2 != 0:
     sum += int
 print(sum)
 
-#5
+# 5
 for int in range(2018,0,-4):
   print(int)
 
-#6
+# 6
 lowNum = 2
 highNum = 9
 multi = 3
