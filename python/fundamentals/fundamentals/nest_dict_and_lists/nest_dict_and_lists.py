22bef77600431a29e6f12ca550411c2c54a7732b
Kristian Tahop <ktahop@gmail.com>
2023-01-09 18:09:41 -0500

    week 1 python

--- python/fundamentals/fundamentals/nest_dict_and_lists/nest_dict_and_lists.py
+++ python/fundamentals/fundamentals/nest_dict_and_lists/nest_dict_and_lists.py
@@ -1,5 +1,5 @@
 # Update Values in Dictionaries and Lists
-x = [ [5,2,3], [10,8,9] ] 
+x = [[5,2,3], [10,8,9]] 
 students = [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'}
@@ -8,26 +8,29 @@ sports_directory = {
     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 }
-z = [ {'x': 10, 'y': 20} ]
+z = [{'x': 10, 'y': 20}]
 
 # 1
 x[1][0] = 15
 print(x)
+
 # 2
 students[0]["last_name"] = "Bryant"
 print(students)
+
 # 3
 sports_directory["soccer"][0] = "Andres"
 print(sports_directory["soccer"])
+
 # 4
 z[0]["y"] = 30
 print(z)
 
-print(" ")
+print(" ") # Used to seperate assignments
 
 # Iterate Through a Listy of Dictionaries
 students = [
-  {'first_name':  'Michael', 'last_name' : 'Jordan'},
+  {'first_name': 'Michael', 'last_name' : 'Jordan'},
   {'first_name': 'John', 'last_name' : 'Rosales'},
   {'first_name': 'Mark', 'last_name' : 'Guillen'},
   {'first_name': 'KB', 'last_name' : 'Tonel'}
